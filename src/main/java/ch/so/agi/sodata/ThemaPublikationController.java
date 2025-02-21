8110824a6052e1ddfd53bba72cb0172a80d1a5d4
Stefan Ziegler <edi.gonzales@gmail.com>
2025-02-21 11:58:09 +0100

    ugly working search

--- src/main/java/ch/so/agi/sodata/ThemaPublikationController.java
+++ src/main/java/ch/so/agi/sodata/ThemaPublikationController.java
@@ -1,5 +1,6 @@
 package ch.so.agi.sodata;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -8,10 +9,13 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 //@RequestMapping("/api/themapublikation")
 public class ThemaPublikationController {
     private Logger log = LoggerFactory.getLogger(this.getClass());
@@ -22,17 +26,32 @@ public class ThemaPublikationController {
         this.themaPublikationService = themaPublikationService;
     }
 
-    //@GetMapping("/api/themapublikation")
     @GetMapping(path = "/api/themapublikation", produces=MediaType.APPLICATION_JSON_VALUE)
     public List<ThemaPublikation> findAll() {
         return themaPublikationService.findAll();
     }
     
+//    @PostMapping(path = "/search") 
+//    public List<ThemaPublikation> search(@RequestParam(name = "search") String search) {
+//        return themaPublikationService.findByFilter(search);
+//    }
+
+    @PostMapping(path = "/search") 
+    public ModelAndView search(@RequestParam(name = "search") String search) {
+        ModelAndView mav = new ModelAndView("search-results");
+        if (search == null || search.length() < 3) {
+            mav.addObject("themaPublikationList", themaPublikationService.findAll());            
+        } else {
+            mav.addObject("themaPublikationList", themaPublikationService.findByFilter(search));            
+        }
+        return mav;
+    }
+
     @GetMapping("/web/themapublikation")
-    public String showAll(Model model) {
-        System.out.println(themaPublikationService.findAll());
-        model.addAttribute("themaPublikationList", themaPublikationService.findAll());
-        return "themapublikation";
+    public ModelAndView showAll() {
+        ModelAndView mav = new ModelAndView("themapublikation");
+        mav.addObject("themaPublikationList", new ArrayList<ThemaPublikation>());
+        return mav;
     }
     
 }
--- src/main/java/ch/so/agi/sodata/ThemaPublikationService.java
+++ src/main/java/ch/so/agi/sodata/ThemaPublikationService.java
@@ -18,6 +18,50 @@ public class ThemaPublikationService {
         this.jdbcClient = jdbcClient;
     }
     
+    // TODO row mapper für ThemaPublikation
+    
+    public List<ThemaPublikation> findByFilter(String filter) {        
+        String stmt = """
+SELECT 
+    t.id, t.titel, t.beschreibung, t.modell, t.publiziertam, t.tags,
+    a.aname, a.abkuerzung, a.abteilung, a.amtimweb, a.zeile1, a.zeile2,
+    a.strasse, a.hausnr, a.plz, a.ort 
+FROM
+    themapublikation_themapublikation AS t
+    LEFT JOIN amt_ AS a
+    ON a.thempblktn_thmpblktion_datenherr = t.T_Id
+WHERE 
+    lower(t.titel) LIKE :filter           
+ORDER BY 
+    t.id
+; 
+                """;
+        return jdbcClient.sql(stmt)
+                .param("filter", "%"+filter+"%")
+                .query((rs, rowNum) -> new ThemaPublikation(
+                      rs.getString("id"),
+                      rs.getString("titel"),
+                      rs.getString("beschreibung"),
+                      rs.getString("modell"),
+                      rs.getString("publiziertam") != null ? LocalDate.parse(rs.getString("publiziertam")) : null,
+                      rs.getString("tags") != null ? rs.getString("tags").split(",") : new String[0],
+                      new Amt(
+                              rs.getString("aname"),
+                              rs.getString("abkuerzung"),
+                              rs.getString("abteilung"),
+                              rs.getString("amtimweb"),
+                              rs.getString("zeile1"),
+                              rs.getString("zeile2"),
+                              rs.getString("strasse"),
+                              rs.getString("hausnr"),
+                              rs.getString("plz"),
+                              rs.getString("ort")
+                              )
+                        ))
+                .list();        
+    }
+    
+    
     public List<ThemaPublikation> findAll() {
         String stmt = """
 SELECT 
